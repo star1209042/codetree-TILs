1a140732514eca012f551aec07ce8020b4125b0a
star1209042 <87555628+star1209042@users.noreply.github.com>
2024-09-29 01:07:50 +0900

    Title: 별 그리기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240929/별 그리기/star-drawing.py
+++ 240929/별 그리기/star-drawing.py
@@ -2,9 +2,7 @@ n= int(input())
 
 for i in range(n):
     print(" "*(n - (i + 1)),end="")
-    print("*"*(2*i+1), end = "")
-    print()
-for i in range(n - 2, -1, -1):
+    print("*"*(2*i+1))
 
+for i in range(n - 2, -1, -1):
     print(' ' * (n - i - 1) + '*' * (2 * i + 1))
-    print(" "*(i-1),end="")
