53381fcb6d2ab69518a608b713bebe997fbe853c
star1209042 <87555628+star1209042@users.noreply.github.com>
2024-11-18 21:49:19 +0900

    Title: 별표 출력하기 12, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241118/별표 출력하기 12/print-star-12.py
+++ 241118/별표 출력하기 12/print-star-12.py
@@ -1,19 +1,15 @@
 n = int(input())
 
 for i in range(n):
-    if i == 0:  # 첫 줄
-        print("* " * n)
-    elif i % 2 == 1:  # 홀수 번째 줄
-        for j in range(n):
-            if j == i // 2 or j == n - i // 2 - 1:
-                print("*", end=" ")
+    for j in range(n):
+        if j % 2 == 0:
+            if i == 0:
+                print("* ", end="")
             else:
-                print(" ", end=" ")
-        print()
-    else:  # 짝수 번째 줄 (빈 공간 줄)
-        for j in range(n):
-            if j == n - i // 2 - 1:
-                print("*", end=" ")
+                print("  ", end="")
+        else:
+            if i <= j:
+                print("* ", end="")
             else:
-                print(" ", end=" ")
-        print()
+                print("  ", end="")
+    print()
